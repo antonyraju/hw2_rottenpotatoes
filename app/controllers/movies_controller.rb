f1611ad4c31399629a6efdeb2f2f236f2594bf4b
antonyraju <antony.raju@gmail.com>
2012-10-13 21:46:17 -0700

    ratings data

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,18 +7,55 @@ class MoviesController < ApplicationController
   end
 
   def index
+    order = params[:order]
+    ratings = params[:ratings]
     @all_ratings = Movie.all_ratings
-    sort_type = params[:sort_type]
-    if sort_type == 'title'
-    @movies = Movie.all(:order => 'title')
-    @hl_title = 'hilite'
-    elsif sort_type == 'release_date'
-    @movies = Movie.all(:order => 'release_date')
-    @hl_release = 'hilite'
+    title_th = title_trd = order_sql = ''
+    redirect = false
+
+    if ratings != nil
+      session[:ratings] = ratings
     else
-    @movies = Movie.all
+      if session.has_key?(:ratings) == false
+        session[:ratings] = { 'G' => 1, 'PG' => 1, 'PG-13' => 1, 'R' => 1 }
+      end
+      redirect = true
     end
 
+    if order != nil
+      session[:order] = order
+    else
+      if session.has_key?(:order) == false
+        session[:order] = ''
+      end
+      redirect = true
+    end
+
+    if redirect == true
+      flash.keep
+      redirect_to movies_path(:order => session[:order], :ratings => session[:ratings])
+      return
+    end
+
+    if ratings == nil
+      @filter_checked = Hash.new
+    else
+      @filter_checked = ratings
+    end
+
+    if order == 'title'
+      order_sql = 'title ASC'
+      @class_th = 'hilite'
+    elsif order == 'release_date'
+      order_sql = 'release_date ASC'
+      @class_trd = 'hilite'
+    end
+
+    if ratings == nil
+      @movies = Movie.all(:order => order_sql)
+    else
+      @movies = Movie.find_all_by_rating(ratings.keys, :order => order_sql)
+    end
   end
 
   def new
@@ -49,5 +86,4 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
-  
 end
